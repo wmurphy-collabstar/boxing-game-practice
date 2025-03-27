6496c92cbea3d808dac91cc6a015d4136b0b25f3
Winona Murphy <wviolinm@gmail.com>
2025-03-26 20:04:19 -0400

    Added objects to represent two players

---
+++ boxing.js
@@ -0,0 +1,13 @@
+// Initial setup
+let player1 = {
+    name: "Player 1",
+    health: 100,
+};
+/* Todo: 
+- Create the player2 Object ; 
+- Create a variable called `currentPlayer` and set it to your `player1` object*/
+let player2 = {
+    name: "Player 2",
+    health: 100
+};
+let currentPlayer = player1;
